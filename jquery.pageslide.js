540e863da5db2bed6de5cd437f7369b4a29dea2d
Derek Perez <derek@caring.com>
2009-06-07 02:02:14 -0700

    resolves issue #10. Moved mechanical CSS requirements directly into the JS, no CSS file required!

--- jquery.pageslide.js
+++ jquery.pageslide.js
@@ -10,15 +10,30 @@
 		    complete:       function(){}
 		}, options);
 		
+		// these are the minimum css requirements for the pageslide elements introduced in this plugin.
+		
+		pageslide_slide_wrap_css = {
+		  position: 'fixed',
+      width: '0',
+      top: '0',
+      height: '100%'
+		};
+		
+		pageslide_body_wrap_css = {
+		  position: 'relative'
+		}
+		
 		function _initialize() {
       // Create and prepare elements for pageSlide
       var psBodyWrap = document.createElement("div");
+      $(psBodyWrap).css(pageslide_body_wrap_css);
       $(psBodyWrap).attr("id","pageslide-body-wrap").width( $("body").width() );
     
       var psSlideContent = document.createElement("div");
       $(psSlideContent).attr("id","pageslide-content").width( settings.width );
     
       var psSlideWrap = document.createElement("div");
+      $(psSlideWrap).css(pageslide_slide_wrap_css);
       $(psSlideWrap).attr("id","pageslide-slide-wrap").append( psSlideContent );
       
       // Wrap and append so that we have the slide containers
