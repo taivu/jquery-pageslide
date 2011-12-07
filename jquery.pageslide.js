fb565b96ce8c791825f9a956ca796daa26550926
Scott Robbin <srobbin@gmail.com>
2011-12-07 07:57:30 -0800

    Created public open function.

--- jquery.pageslide.js
+++ jquery.pageslide.js
@@ -23,148 +23,163 @@
                                   .css( 'display', 'none' )
                                   .appendTo( $('body') );
     }
+    
+    /*
+     * Private methods 
+     */
+    function _load( url, useIframe ) {
+        // Are we loading an element from the page or a URL?
+        if ( url.indexOf("#") === 0 ) {                
+            // Load a page element                
+            $(url).clone(true).appendTo( $pageslide.empty() ).show();
+        } else {
+            // Load a URL. Into an iframe?
+            if( useIframe ) {
+                var iframe = $("<iframe />").attr({
+                                                src: url,
+                                                frameborder: 0,
+                                                hspace: 0
+                                            })
+                                            .css({
+                                                width: "100%",
+                                                height: "100%"
+                                            });
+                
+                $pageslide.html( iframe );
+            } else {
+                $pageslide.load( url );
+            }
+            
+            $pageslide.data( 'localEl', false );
+            
+        }
+    }
+    
+    // Function that controls opening of the pageslide
+    function _start( direction, speed ) {
+        var slideWidth = $pageslide.outerWidth( true ),
+            bodyAnimateIn = {},
+            slideAnimateIn = {};
+        
+        // If the slide is open or opening, just ignore the call
+        if( $pageslide.is(':visible') || _sliding ) return;	        
+        _sliding = true;
+                                                                    
+        switch( direction ) {
+            case 'left':
+                $pageslide.css({ left: 'auto', right: '-' + slideWidth + 'px' });
+                bodyAnimateIn['margin-left'] = '-=' + slideWidth;
+                slideAnimateIn['right'] = '+=' + slideWidth;
+                break;
+            default:
+                $pageslide.css({ left: '-' + slideWidth + 'px', right: 'auto' });
+                bodyAnimateIn['margin-left'] = '+=' + slideWidth;
+                slideAnimateIn['left'] = '+=' + slideWidth;
+                break;
+        }
+                    
+        // Animate the slide, and attach this slide's settings to the element
+        $body.animate(bodyAnimateIn, speed);
+        $pageslide.show()
+                  .animate(slideAnimateIn, speed, function() {
+                      _sliding = false;
+                  });
+    }
       
-    // Declare pageslide
-    $.fn.pageslide = $.fn.pageSlide = function(options) {
+    /*
+     * Declaration 
+     */
+    $.fn.pageslide = function(options) {
         var $elements = this;
-        
-        // Default settings
-        var _settings = {
-            speed:      200,        // Accepts standard jQuery effects speeds (i.e. fast, normal or milliseconds)
-            direction:  'right',    // Accepts 'left' or 'right'
-            modal:      false,      // If set to true, you must explicitly close pageslide using $.pageslide.close();
-            iframe:     true        // By default, linked pages are loaded into an iframe. Set this to false if you don't want an iframe.
-        };
-        
-        // Extend the settings with those the user has provided
-        if(options && typeof options == 'object') $.extend(_settings, options);
         
         // On click
         $elements.click( function(e) {
             var $self = $(this),
-                href = $self.attr('href'),
-                visible = $pageslide.is(':visible');
+                settings = $.extend({ href: $self.attr('href') }, options);
             
             // Prevent the default behavior and stop propagation
             e.preventDefault();
             e.stopPropagation();
             
-            // Are we trying to open in different direction?
-            if( visible && $pageslide.data( 'direction' ) != _settings.direction) {
-                $.pageslide.close(function(){
-                    _load( href );
-                    _open();
-                });
-            } else if ( visible && $self[0] == _lastCaller ) {
+            if ( $pageslide.is(':visible') && $self[0] == _lastCaller ) {
                 // If we clicked the same element twice, toggle closed
                 $.pageslide.close();
-            } else {                
-                _load( href );
-                if( $pageslide.is(':hidden') ) {
-                    _open();
-                }
-            }
-            
-            // Record the last element to trigger pageslide
-            _lastCaller = $self[0];         
-        });
-        
-        function _load( url ) {
-            // Are we loading an element from the page or a URL?
-            if ( url.indexOf("#") === 0 ) {                
-                // Load a page element                
-                $(url).clone(true).appendTo( $pageslide.empty() ).show();
-            } else {
-                // Load a URL. Into an iframe?
-                if( _settings.iframe ) {
-                    var iframe = $("<iframe />").attr({
-                                                    src: url,
-                                                    frameborder: 0,
-                                                    hspace: 0
-                                                })
-                                                .css({
-                                                    width: "100%",
-                                                    height: "100%"
-                                                });
-                    
-                    $pageslide.html( iframe );
-                } else {
-                    $pageslide.load( url );
-                }
-                
-                $pageslide.data( 'localEl', false );
-                
-            }
-        }
-        
-        // Function that controls opening of the pageslide
-        function _open() {
-            var slideWidth = $pageslide.outerWidth( true ),
-                bodyAnimateIn = {},
-                slideAnimateIn = {};
-            
-            // If the slide is open or opening, just ignore the call
-	        if( $pageslide.is(':visible') || _sliding ) return;	        
-	        _sliding = true;
-                                                                        
-            switch(_settings.direction) {
-                case 'left':
-                    $pageslide.css({ left: 'auto', right: '-' + slideWidth + 'px' });
-                    bodyAnimateIn['margin-left'] = '-=' + slideWidth;
-                    slideAnimateIn['right'] = '+=' + slideWidth;
-                    break;
-                default:
-                    $pageslide.css({ left: '-' + slideWidth + 'px', right: 'auto' });
-                    bodyAnimateIn['margin-left'] = '+=' + slideWidth;
-                    slideAnimateIn['left'] = '+=' + slideWidth;
-                    break;
-            }
-                        
-            // Animate the slide, and attach this slide's settings to the element
-            $body.animate(bodyAnimateIn, _settings.speed);
-            $pageslide.data( _settings )
-                      .show()
-                      .animate(slideAnimateIn, _settings.speed, function() {
-                          _sliding = false;
-                      });
-        }
-                       
+            } else {                 
+                // Open
+                $.pageslide( settings );
+
+                // Record the last element to trigger pageslide
+                _lastCaller = $self[0];
+            }       
+        });                   
 	};
 	
-	// Public methods
-	$.pageslide = $.pageSlide = {
+	/*
+     * Default settings 
+     */
+    $.fn.pageslide.defaults = {
+        speed:      200,        // Accepts standard jQuery effects speeds (i.e. fast, normal or milliseconds)
+        direction:  'right',    // Accepts 'left' or 'right'
+        modal:      false,      // If set to true, you must explicitly close pageslide using $.pageslide.close();
+        iframe:     true,       // By default, linked pages are loaded into an iframe. Set this to false if you don't want an iframe.
+        href:       null        // Override the source of the content. Optional in most cases, but required when opening pageslide programmatically.
+    };
+	
+	/*
+     * Public methods 
+     */
+	
+	// Open the pageslide
+	$.pageslide = function( options ) {	    
+	    // Extend the settings with those the user has provided
+        var settings = $.extend({}, $.fn.pageslide.defaults, options);
 	    
-	    // Closes pageslide
-	    close: function( callback ) {
-	        var $pageslide = $('#pageslide'),
-	            slideWidth = $pageslide.outerWidth( true ),
-	            speed = $pageslide.data( 'speed' ),
-                bodyAnimateIn = {},
-                slideAnimateIn = {}
-                	        
-	        // If the slide isn't open, just ignore the call
-	        if( $pageslide.is(':hidden') || _sliding ) return;	        
-	        _sliding = true;
-	        
-	        switch( $pageslide.data( 'direction' ) ) {
-                case 'left':
-                    bodyAnimateIn['margin-left'] = '+=' + slideWidth;
-                    slideAnimateIn['right'] = '-=' + slideWidth;
-                    break;
-                default:
-                    bodyAnimateIn['margin-left'] = '-=' + slideWidth;
-                    slideAnimateIn['left'] = '-=' + slideWidth;
-                    break;
-            }
-            
-            $pageslide.animate(slideAnimateIn, speed);
-            $body.animate(bodyAnimateIn, speed, function() {
-                $pageslide.hide();
-                _sliding = false;
-                if( typeof callback != 'undefined' ) callback();
+	    // Are we trying to open in different direction?
+        if( $pageslide.is(':visible') && $pageslide.data( 'direction' ) != settings.direction) {
+            $.pageslide.close(function(){
+                _load( settings.href, settings.iframe );
+                _start( settings.direction, settings.speed );
             });
-	    }
+        } else {                
+            _load( settings.href, settings.iframe );
+            if( $pageslide.is(':hidden') ) {
+                _start( settings.direction, settings.speed );
+            }
+        }
+        
+        $pageslide.data( settings );
 	}
+	
+	// Close the pageslide
+	$.pageslide.close = function( callback ) {
+        var $pageslide = $('#pageslide'),
+            slideWidth = $pageslide.outerWidth( true ),
+            speed = $pageslide.data( 'speed' ),
+            bodyAnimateIn = {},
+            slideAnimateIn = {}
+            	        
+        // If the slide isn't open, just ignore the call
+        if( $pageslide.is(':hidden') || _sliding ) return;	        
+        _sliding = true;
+        
+        switch( $pageslide.data( 'direction' ) ) {
+            case 'left':
+                bodyAnimateIn['margin-left'] = '+=' + slideWidth;
+                slideAnimateIn['right'] = '-=' + slideWidth;
+                break;
+            default:
+                bodyAnimateIn['margin-left'] = '-=' + slideWidth;
+                slideAnimateIn['left'] = '-=' + slideWidth;
+                break;
+        }
+        
+        $pageslide.animate(slideAnimateIn, speed);
+        $body.animate(bodyAnimateIn, speed, function() {
+            $pageslide.hide();
+            _sliding = false;
+            if( typeof callback != 'undefined' ) callback();
+        });
+    }
 	
 	/* Events */
 	
